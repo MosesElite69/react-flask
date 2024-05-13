67a54d3340536c37712eca5b2f588b350a2bd09b
Elliot <elliotedwinmoses2020@gmail.com>
2024-05-13 16:02:17 +0200

    Deployment

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,33 @@
 # UPDATE THIS FILE FOR DEPLOYMENT
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db" # NAME DATABASE WITH .db
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friends.db"  # NAME DATABASE WITH .db
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-import routes # because routes.py is in the same folder and not returning anything
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+
+# Server static files from the "dist" folder under the "frontend" directory
+@app.route("/", defaults={"filename": ""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
+
+
+# api routes
+
+import routes  # because routes.py is in the same folder and not returning anything
 
 # CREATE DATABASE
 with app.app_context():
@@ -19,4 +35,3 @@ with app.app_context():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
